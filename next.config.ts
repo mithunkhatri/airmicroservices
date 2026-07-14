06941b7a77f2084667977be51b9a1ab29e2aed02
Mithun Khatri <mithun.khatri@whitebox-services.eu>
2026-07-14 16:15:28 +0530

    add simulation of construction pm ai project

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/simulation/:slug",
+        destination: "/simulation/:slug.html",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
